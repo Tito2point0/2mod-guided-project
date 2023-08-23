26e3ab89b0be13bada29b37a251c4668cb84d0b0
sanjiro <defconx00@gmail.com>
2023-08-22 21:33:17 -0400

    search button applied

---
+++ doggos/src/SearchForm.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+
+
+class SearchForm extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            inputValue: ''
+        }
+    }
+
+
+
+    render(){
+        return(
+            <div >
+            <input placeholder= 'breed' value={this.state.inputValue} onChange={(e) => this.setState({ inputValue: e.target.value})} />
+            <button onClick={()=> this.props.searchDogs(this.state.inputValue)}> Submit</button>
+            </div>
+
+        )
+    }
+}
+export default SearchForm;
